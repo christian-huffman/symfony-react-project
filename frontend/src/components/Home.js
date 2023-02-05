9ec8513f55f18934613ecc367ebdc4fd85adf257
Christian Huffman <christian.huffman.88@gmail.com>
2023-02-05 18:31:40 -0500

    feat: added front end

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Route, Routes, Navigate, Link, withRouter } from "react-router-dom";
+import Users from "./Users";
+import Posts from "./Posts";
+
+export default function Home(props) {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link className={"navbar-brand"} to={"/"}>
+          {" "}
+          Symfony React Project{" "}
+        </Link>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link className={"nav-link"} to={"/posts"}>
+                {" "}
+                Posts{" "}
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className={"nav-link"} to={"/users"}>
+                {" "}
+                Users{" "}
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/users" element={<Users />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/" element={<Navigate replace to="/users" />} />
+      </Routes>
+    </div>
+  );
+}
